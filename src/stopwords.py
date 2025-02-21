9a1fa8e8aefdaf69cbd3c546a5adcfe2794c7c3c
isatyamks <isatyamks@gmail.com>
2025-02-21 12:05:02 +0530

    Refactor stopwords handling to improve text processing and update function calls for consistency

--- src/stopwords.py
+++ src/stopwords.py
@@ -2,15 +2,11 @@ import nltk
 from nltk.corpus import stopwords
 import string
 
-def remove_stop_words (text):
     
-    def remove_stopwords(text, stopwords_file):
-        with open(stopwords_file, 'r', encoding='utf-8') as file:
-            stop_words = set(file.read().splitlines())
-        words = nltk.word_tokenize(text)
-        filtered_words = [word for word in words if word.lower() not in stop_words and word not in string.punctuation]
-        return ' '.join(filtered_words)
-    stopwords_file = 'src\stopwords.txt'
-    clean_text = remove_stopwords(text, stopwords_file)
-    return clean_text
+def remove_stopwords(text, stopwords_file):
+    with open(stopwords_file, 'r', encoding='utf-8') as file:
+        stop_words = set(file.read().splitlines())
+    words = nltk.word_tokenize(text)
+    filtered_words = [word for word in words if word.lower() not in stop_words and word not in string.punctuation]
+    return ' '.join(filtered_words)
 
